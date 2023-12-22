e114f46d97d54830fcc2ddf8c3e0ec1393dd53d9
Herman <hermanvasilkouskiy@gmail.com>
2023-12-22 22:40:37 +0300

    feat: complete Date task

--- src/what-season.js
+++ src/what-season.js
@@ -12,7 +12,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  throw new NotImplementedError('Not implemented');
+  if (!date) return 'Unable to determine the time of year!';
+  if (!(date instanceof Date)) throw new Error('Invalid date!');
+  const month = date.getMonth();
+
+
+  if (month >= 11 || month <= 1) return 'winter';
+  if (month >= 2 && month <= 4) return 'spring';
+  if (month >= 5 && month <= 7) return 'summer';
+  if (month >= 8 && month <= 10) return 'autumn';
 }
 
 module.exports = {
